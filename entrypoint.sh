0d39e9c3224995e45271c55a5e929f93bb0a05f5
Randy Hommel <rhommel@infiniticg.com>
2017-03-22 17:24:51 -0700

    Fixed sed command, invalid option provided -s should have been -e

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ ip=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 search="s#INSTANCEID#$ip#g"
 
-sed -i -s $search /usr/share/nginx/html/index.html
+sed -i -e $search /usr/share/nginx/html/index.html
