11b2ca0ec37670ba2a09d44e37f48bdbe3cfd1ba
Randy Hommel <rhommel@infiniticg.com>
2017-03-22 17:41:28 -0700

    added nginx to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-#ip=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+ip=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 if [ -z "$ip" ]; then
 	ip='&nbsp;'
-else
+fi
 
 search="s#INSTANCEID#$ip#g"
 
 sed -i -e $search /usr/share/nginx/html/index.html
+
+/usr/sbin/nginx -g "daemon off;"
