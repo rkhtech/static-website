856269a91ff6583bf6e842a36d3deaf073806f4a
Randy Hommel <rhommel@infiniticg.com>
2017-04-21 15:17:02 -0700

    Added timeout to curl commands

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ RUN apk update && apk add curl
 ADD index.html /usr/share/nginx/html/index.html
 ADD entrypoint.sh /entrypoint.sh
 
+EXPOSE 80
+
 ENTRYPOINT /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-ip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+id=$(curl -s --connect-timeout 1 http://169.254.169.254/latest/meta-data/instance-id)
+ip=$(curl -s --connect-timeout 1 http://169.254.169.254/latest/meta-data/local-ipv4)
 
 if [ -z "$ip" ]; then
+	echo "INFO: Container is not running on EC2."
 	ip='&nbsp;'
 fi
 if [ -z "$id" ]; then
@@ -15,4 +16,6 @@ search2="s#LOCALIP#$ip#g"
 
 sed -i -e $search1 -e $search2 /usr/share/nginx/html/index.html
 
+echo "Starting nginx:"
 /usr/sbin/nginx -g "daemon off;"
+
